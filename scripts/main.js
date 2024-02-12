544dd217cc80e64c1ced5b8945b013e3de72dbfe
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-12 17:44:18 +0100

    swap two images interchangeably

--- scripts/main.js
+++ scripts/main.js
@@ -60,3 +60,141 @@ document.querySelector("#btn").addEventListener("click", () =>{
     alert("praise the LORD oh my SOUL")
 })
 
+
+let firstImg = document.querySelector(".first_img")
+let secondImg = document.querySelector(".second_img")
+
+
+// images to dynamically change
+img = document.querySelectorAll(".image_container img")
+
+
+let firstImgSrc = firstImg.getAttribute('src');
+let tmp = firstImg;
+let secondImgSrc = secondImg.getAttribute('src');
+
+firstImg.addEventListener('click', () => 
+{
+    firstImg.setAttribute('src', secondImgSrc)
+    
+    secondImg.setAttribute('src',firstImgSrc)
+})
+
+
+
+secondImg.addEventListener('click', () => 
+{
+
+    if (firstImg.getAttribute('src') === firstImg.getAttribute('src'))
+    {
+        secondImg.setAttribute('src', firstImgSrc)
+    }else{
+      firstImg.setAttribute('src', secondImgSrc)
+    }
+
+    
+})
+    // if (firstImgSrc === secondImgSrc)
+    // {
+    //     console.log("it has changed")
+    // }else{
+    //     console.log("it has not changed")
+    // }
+
+    // let firstImgSwap = firstImg.setAttribute('src', firstImgSrc)
+    // let second_img = secondImg.setAttribute('src',secondImgSrc)
+// })
+
+
+// for (let i = 0; i < img.length; i++)
+// {
+
+
+   
+
+//     // if (img[i].classList == 'first_img')
+//     // {
+//     //   firstImgSrc = img[i].getAttribute('src')
+//     //   console.log(firstImgSrc)
+//     // }
+//     // else{
+//     //     secondImgSrc = img[i].getAttribute('src')
+//     //     console.log("this is the second cat",secondImgSrc)
+//     // }
+
+
+//     // img[i].addEventListener('click', () => {
+    
+        
+//     // })
+    
+
+//     // let imageList = []
+
+//     // console.log(img)
+
+//     // let imageAttri = []
+
+//     // let imageSrc = img[i].getAttribute('src')
+//     // let firstImage = imageSrc[i]
+//     // // imageAttri.push(imageSrc[i])
+
+//     // console.log(firstImage)
+//     // // console.log(imageAttri)
+//     // console.log(imageSrc)
+//     // let imgSrc = img[i].getAttribute('src')
+//     // let imgSrc2 = img[1].getAttribute('src')
+//     // console.log("this is cat1",imgSrc)
+//     // console.log("this is the cat",imgSrc2)
+
+//     //   img[0].addEventListener("click", () => {
+//     //     let imgSrc = img[i].getAttribute('src')
+//     //     console.log(imgSrc)
+       
+//     //   })
+
+//     //   img[1].addEventListener("click", () => {
+//     // //     let imgSrc = img[i].getAttribute('src')
+//     //     console.log(imgSrc)
+       
+//     //   })
+
+
+
+    
+// }
+
+
+
+
+// // addeventlistner
+
+// // imgContainer.addEventListener('click', (e) =>{
+
+// //    if (e.target.nodeName === 'IMG')
+// //    {
+//         // get the image attribute
+//     //    imageSrc = e.target.getAttribute('src')
+//     //    console.log(imageSrc)
+
+//         // for(i < 0; i < imgContainer.length; i++)
+//         // {
+//         //     console.log(imgContainer[i])
+//         // }
+
+//         // for (let index = 0; index < array.length; index++) {
+//         //     const element = array[index];
+            
+//         // }
+
+//        // swap their attribute.
+//     //    if (imageSrc === 'images/cat1.jpeg')
+//     //    {
+//     //         // set the attibute to change the image
+//     //         imageSrc = e.target.setAttribute('src', 'images/cat2.jpeg')
+//     //    }else
+//     //    {
+//     //         imageSrc = e.target.setAttribute('src', 'images/cat1.jpeg')
+//     //    }
+
+
