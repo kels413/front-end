cb21d2e631b5daa52e6c4c8899e1bc06e7546cd7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-12 14:18:37 +0100

    learning js

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+heading = document.getElementById('kellys')
+heading.textContent = "Kelly okoye"
+console.log(heading)
+
+const studentName = '1'
+const book = 'cup'
+const sum = studentName + book
+console.log(sum)
+
+
