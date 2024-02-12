ef6d963864ad8fa97accca969b44fe2e7af99a5c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-12 14:23:05 +0100

    for loops

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,12 @@ const book = 'cup'
 const sum = studentName + book
 console.log(sum)
 
-
+//arrays
 let studentNames = ["book", "cup", "okoye", "eye"]
 
+/*
+For loops in js
+*/
 for (let index = 0; index < studentNames.length; index++) {
     console.log(studentNames[index])
 }
